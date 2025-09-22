04c6ad8c160b5f71fc9f041e89f4c5d53363f5ca
Ramzisyuhada <ramzi.syuhada@mhs.itenas.ac.id>
2025-09-23 00:39:54 +0700

    Recipe

---
+++ Assets/Resources/Script/UI/RecipeManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecipeManager : MonoBehaviour
+{
+
+    [SerializeField] private Material _Material;
+
+    [SerializeField] List<Sprite> _Sprites;
+
+
+}
